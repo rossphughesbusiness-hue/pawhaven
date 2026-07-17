f00daec2c6567e75e9429ffac9be5a0be9846c75
rossphughesbusiness-hue <rossphughesbusiness@gmail.com>
2026-07-17 12:09:19 -0400

    fix: allow /api/feed and /api/feed.xml in robots.txt (Google Shopping feed was crawl-blocked)

--- src/app/robots.js
+++ src/app/robots.js
@@ -2,7 +2,7 @@ export default function robots() {
   return {
     rules: {
       userAgent: '*',
-      allow: ['/', '/api/feed/'],
+      allow: ['/', '/api/feed', '/api/feed.xml', '/api/feed/'],
       disallow: ['/api/', '/checkout/'],
     },
     sitemap: 'https://pawhavenpets.org/sitemap.xml',
